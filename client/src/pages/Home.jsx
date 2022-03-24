1b535cfcb4ae473272a0c1f07311050ff4d0f41f
manish-mehra <manishmehra1469@gmail.com>
2022-03-24 16:45:38 +0530

    chore: configure tailwind

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function Home() {
   return (  
     <div>
-        <h1>Welcome to Thumstack Spoon</h1>
+        <h1 className="text-3xl font-bold underline">Welcome to Thumstack Spoon</h1>
         <Link to="/place-order">Enter the restaurant</Link>
     </div>
   ) 
